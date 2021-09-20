3814c481e0f49e14320560f51df346d87b5dfee0
rajgopal <rajgopal.c@gmail.com>
2021-09-20 20:46:34 +0530

    Print and Comments

---
+++ printHello.py
@@ -0,0 +1,57 @@
+#This is a python comment.
+
+#Below line is going to print a String.
+
+print("Hello, World!")
+
+#Below line is going to print an expression
+
+print("NatWest" * 5) #NatWest * 5 is an expression which is evaluated first
+
+#With new line
+print("NatWest\n" * 5)
+
+#Print adds an extra new line character.
+print(".")
+print("hello\n"*5,end="")
+print(".")
+
+
+
+
+
+
+
+#TryOuts
+
+
+# print("  ^     ^ ")
+# print("(   \/    )")
+# print("  \     /")
+# print("    \ / ")
+# print("     .")
+
+# print("    ******       ******        ")
+# print("   **********   **********     ")
+# print(" ************* *************   ")
+# print(" ***************************** ")
+# print("  ***************************  ")
+# print("   ***********************     ")
+# print("     *******************       ")
+# print("       ***************         ")
+# print("         ***********           ")
+# print("          *******              ")
+# print("            ***                ")
+# print("             *                 ")
+#
+# print("\u2764\uFE0F")
+#
+#
+#
+# for row in range(6):
+#     for col in range(7):
+#         if (row==0 and col %3 !=0)or(row==1 and col %3==0) or(row-col==2) or(row+col==8):
+#             print("*",end=" ")
+#         else:
+#             print(end=" ")
+#     print()
